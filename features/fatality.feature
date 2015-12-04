e3a087e98c70586c9d9dc23481ee2ca4ab9e298f
Your Name <you@example.com>
2015-12-04 11:38:19 -0500

    avance fatality

---
+++ features/fatality.feature
@@ -0,0 +1,10 @@
+Feature:
+  Como jugador
+  Quiero que los personajes puedan realizar falatity
+  Para terminar de una manera sangrienta con mi contrincante
+
+Scenario:
+
+  Given la pelea termino
+  And se realizo la combinacion de golpes para fatality
+  Then debo ver el mensaje "FATALITY"
--- features/step_definitions/my_steps.rb
+++ features/step_definitions/my_steps.rb
@@ -7,7 +7,6 @@ Then(/^debo ver el mensaje "(.*?)"$/) do |mensaje|
 end
 
 Given(/^que un jugador tiene cero de vida$/) do
-
   click_button("PA")
   click_button("PA")
   click_button("PA")
@@ -38,4 +37,15 @@ end
 When(/^presione "(.*?)"$/) do |comando|
   click_button(comando)
 
+end
+
+
+Given(/^la pelea termino$/) do
+  visit '/finish'
+end
+
+Given(/^se realizo la combinacion de golpes para fatality$/) do
+  click_button("PA")
+  click_button("PA")
+  click_button("PA")
 end
